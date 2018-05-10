9e2118749ca79fcc3f9d0cad9b9787b2f3932567
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-10 18:52:06 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 # Add code here!
 def prime?(number) #define a method, prime?() that takes in an integer argument
   # Define what a prime number is: a number that is only divisible by itself or by 1 (thank you Prof Google)
-  # if number <=1
-  #   false #because negative numbers are not prime
-  # else
+  if number <=1
+    false #because negative numbers are not prime
+  else
   # otherwise if none of the numbers in the range can be divided by 2, or itself, then false
     (2..number/2).none? { |i| number % i == 0}
   # For each of the numbers in the range 2 to number/2...
