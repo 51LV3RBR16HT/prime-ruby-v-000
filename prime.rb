1b1fd58fa117a1d534a49b8bded59dab9048ca34
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-10 18:23:07 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(number) #defining a method, prime?() that takes in an integer argument
-  if number == 1 || number == 2 || number % 3
+  if number == 2 || number % 3
     true
   else
     number <= 0
@@ -8,14 +8,8 @@ def prime?(number) #defining a method, prime?() that takes in an integer argumen
   end
 end
 
-# if integer is prime
-#   true
-# else
-#   false
-
-
 # if integer is prime, return true
 # if integer is not prime, return false
 # turn a range into an array so it can be iterated over
-# research alggorithms (...?)
+# research algorithms (...?)
 # look into Benchmarking (...?)
