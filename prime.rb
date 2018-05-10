cce5c8c30202f27b8692aef842d5c6d076eccf90
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-10 18:09:24 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 # Add  code here!
-def prime?(number)
+def prime?(number) #defining a method, prime?() that takes in an integer argument
 # if integer is prime, return true
 # if integer is not prime, return false
 end
