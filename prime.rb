7358af00beca4bc93fb4666cbca721d2a091bb79
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-10 18:12:45 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,12 @@
 # Add  code here!
 def prime?(number) #defining a method, prime?() that takes in an integer argument
 
+
+
 # if integer is prime, return true
 # if integer is not prime, return false
-
 # turn a range into an array so it can be iterated over
-
 # research alggorithms (...?)
 # look into Benchmarking (...?)
-
 
 end
